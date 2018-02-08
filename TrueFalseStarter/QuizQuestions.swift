c9bb23fd547790ec5e700d19012230f333d0fd87
Jamie <jamie@butterstone.com>
2018-02-08 18:19:09 +0000

    added questions object

---
+++ TrueFalseStarter/QuizQuestions.swift
@@ -0,0 +1,34 @@
+//
+//  QuestionProvider.swift
+//  TrueFalseStarter
+//
+//  Created by Jamie MacLean on 08/02/2018.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct QuestionFormat {
+    var question: String
+    var answerChoices: [String]
+    var answer: String
+}
+
+
+struct QuizQuestions {
+    var questions :[QuestionFormat] = [
+        QuestionFormat(
+            question: "The movie 'The Song Remains The Same' featured which artists?",
+            answerChoices: ["Led Zeppelin", "The Who", "The Band", "U2"],
+            answer: "Led Zeppelin "),
+        QuestionFormat(
+            question: "Which Beatles' song contains the lyric, 'When I was young, so much younger than today'?",
+            answerChoices: ["Eight Days A Week", "Blackbird", "All You Need Is Love", "Help"],
+            answer: "Help"),
+        QuestionFormat(
+            question: "What was the name of Keith Moon's only solo album?",
+            answerChoices: ["The Full Moon", "Moon The Loon", "Moon The Loon", "Two Sides Of The Moon"],
+            answer: "Two Sides Of The Moon"),
+        ]
+}
+
--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -12,6 +12,7 @@ import AudioToolbox
 
 class ViewController: UIViewController {
     
+    let quizQuestions = QuizQuestions()
     let questionsPerRound = 4
     var questionsAsked = 0
     var correctQuestions = 0
@@ -19,12 +20,12 @@ class ViewController: UIViewController {
     
     var gameSound: SystemSoundID = 0
     
-    let trivia: [[String : String]] = [
-        ["Question": "Only female koalas can whistle", "Answer": "False"],
-        ["Question": "Blue whales are technically whales", "Answer": "True"],
-        ["Question": "Camels are cannibalistic", "Answer": "False"],
-        ["Question": "All ducks are birds", "Answer": "True"]
-    ]
+//    let trivia: [[String : String]] = [
+//        ["Question": "Only female koalas can whistle", "Answer": "False"],
+//        ["Question": "Blue whales are technically whales", "Answer": "True"],
+//        ["Question": "Camels are cannibalistic", "Answer": "False"],
+//        ["Question": "All ducks are birds", "Answer": "True"]
+//    ]
     
     @IBOutlet weak var questionField: UILabel!
     @IBOutlet weak var trueButton: UIButton!
@@ -46,9 +47,9 @@ class ViewController: UIViewController {
     }
     
     func displayQuestion() {
-        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: trivia.count)
-        let questionDictionary = trivia[indexOfSelectedQuestion]
-        questionField.text = questionDictionary["Question"]
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: quizQuestions.questions.count)
+       // let questionDictionary = trivia[indexOfSelectedQuestion]
+        questionField.text = quizQuestions.questions[indexOfSelectedQuestion].question
         playAgainButton.isHidden = true
     }
     
@@ -68,8 +69,8 @@ class ViewController: UIViewController {
         // Increment the questions asked counter
         questionsAsked += 1
         
-        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
-        let correctAnswer = selectedQuestionDict["Answer"]
+        let selectedQuestionDict = quizQuestions.questions[indexOfSelectedQuestion]
+        let correctAnswer = selectedQuestionDict.answer
         
         if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
             correctQuestions += 1
